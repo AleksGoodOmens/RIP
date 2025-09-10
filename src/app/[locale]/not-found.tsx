cdc5f68d9508d376c40e0f39c6201cc2644b4f80
Aleks Gomeniuk <agsfinks@gmail.com>
2025-09-10 09:42:19 +0100

    feat: add candle picture and translation title/button

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,4 +1,24 @@
-const NotFound = () => {
-  return <div>page not found</div>;
+import Image from 'next/image';
+import { getTranslations } from 'next-intl/server';
+
+import { Text } from '@/components';
+import { Link } from '@/i18n/navigation';
+
+import candleImage from './images/candle.gif';
+
+const NotFound = async () => {
+  const t = await getTranslations('notFound');
+
+  return (
+    <section className="grid place-content-center gap-4 p-4">
+      <Text as={'h1'} size={'xl'}>
+        {t('header')}
+      </Text>
+      <Link href={'/'} className="uppercase">
+        {t('button')}
+      </Link>
+      <Image alt="candle" src={candleImage} width={200} className="mx-auto rounded-4xl" />
+    </section>
+  );
 };
 export default NotFound;
