90c2d75601d42d8b371216d4fd153939e805ec5c
Aleks Gomeniuk <agsfinks@gmail.com>
2025-09-11 11:08:15 +0100

    refactor: change default export of local switcher component for re-export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './mode-toggle/ModeToggle';
 export * from './theme-provider/ThemeProvider';
 export * from './input-field/InputField';
 export * from './text/Text';
+export * from './local-switcher/LocalSwitcher';
